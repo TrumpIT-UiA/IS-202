9873b8b0daf6d36336c95c82473631054de5edf9
Konflux <marign16@student.uia.no>
2017-09-28 12:56:02 +0200

    Kommentarer

--- src/main/java/slit/main.java
+++ src/main/java/slit/main.java
@@ -1,20 +1,30 @@
 package slit;
 
 import javax.persistence.EntityManager;
-        import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-class slit {
+class main {
 
     public static void main(String[] args) {
+        //Oppretter et Entity manager factory med databasen definert i META-INF\persistence.xml (SlitBase)
         EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("SlitBase");
+        //Oppretter en EntityManager ut ifra EntityManager fabrikken, slik at fabrikken tar hånd om denne.
         EntityManager manager = emfactory.createEntityManager();
+
+        //Lager en transaction-variabel som vi bruker senere
         EntityTransaction transaction = null;
+
+        //Lager en transaction utifra EntityManageren og starter den.
         transaction = manager.getTransaction();
         transaction.begin();
-        Student test = new Student("javel", "sedetveien 9");
-        manager.persist(test);
+
+            //Oppretter et nytt student-objekt og gjør det klart for lagring (persist) i databasen
+            Student test = new Student("javel", "sedetveien 9");
+            manager.persist(test);
+
+        //Comitter dataen til databasen og lukker fabrikken + manageren
         transaction.commit();
         manager.close();
         emfactory.close();
