b2ae4844aab82ccdf002bc17db5451b1e07369b1
emilr15 <emilr15@uia.no>
2017-09-28 09:26:15 +0200

    Ordnet egen branch

--- JPA-test/src/main/java/slit/main.java
+++ JPA-test/src/main/java/slit/main.java
@@ -1,7 +1,7 @@
 package slit;
 
 import javax.persistence.EntityManager;
-        import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
