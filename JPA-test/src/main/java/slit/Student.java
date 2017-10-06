8f2174930e6bf925b3f6c3bbde4cb52570d3c573
emilr15 <emilr15@uia.no>
2017-10-06 10:35:04 +0200

    Endringer

--- JPA-test/src/main/java/slit/Student.java
+++ JPA-test/src/main/java/slit/Student.java
@@ -1,6 +1,9 @@
 package slit;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @Entity
 public class Student {
